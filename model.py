c75b0401cd535127221b8a2acb399dfe062a79da
Susunu Tabuchi <swk22711@gmail.com>
2020-06-06 17:40:18 +0900

    add

--- model.py
+++ model.py
@@ -473,13 +473,11 @@ if __name__ == "__main__":
     # 対象のURL
     # url = "https://urasunday.com/title/55"
     url_list = [
-        'https://urasunday.com/title/98',
-        'https://urasunday.com/title/42',
-        'https://urasunday.com/title/701',
-        'https://urasunday.com/title/701/123977',
-        'https://urasunday.com/title/177',
         'https://urasunday.com/title/561',
-        'https://urasunday.com/title/24'
+        'https://urasunday.com/title/24',
+        'https://urasunday.com/title/349',
+        'https://urasunday.com/title/659',
+        'https://urasunday.com/title/42'
     ]
     
     try:

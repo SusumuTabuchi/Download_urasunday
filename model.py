56299636d92b6b990729cbf2bd8cf41a6f4cd2a3
Susunu Tabuchi <swk22711@gmail.com>
2020-07-12 00:23:58 +0900

    漫画追加-20200712-不足分追加

--- model.py
+++ model.py
@@ -473,13 +473,7 @@ if __name__ == "__main__":
     # 対象のURL
     # url = "https://urasunday.com/title/55"
     url_list = [
-        'https://urasunday.com/title/701',
-        'https://urasunday.com/title/177',
-        'https://urasunday.com/title/24',
-        'https://urasunday.com/title/55',
-        'https://urasunday.com/title/659',
-        'https://urasunday.com/title/66',
-        'https://urasunday.com/title/98'
+        'https://urasunday.com/title/701/126704'
     ]
     
     try:
